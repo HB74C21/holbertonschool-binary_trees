033bf2088f4f25d04b001721cefb7ceb74596f64
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2024-01-09 11:00:57 +0000

    fix error file 5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/*---------------------------------------------------------------------------*/
+		/*BINARY_TREE_IS_ROOT*/
+/*---------------------------------------------------------------------------*/
+
+/**
+ * binary_tree_is_root - Function that checks if a given node is a root.
+ * @node: Pointer to the node to check.
+ *
+ * Return: 1 if node is a root, otherwise 0
+*/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	int is_root = 1;
+
+	if ((node->parent == NULL) || (node == NULL))
+		is_root = 0;
+	return (is_root);
+}
