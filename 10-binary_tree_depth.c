816b031cd7cfe0a6588ec4c2daade7dd97a582df
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2024-01-09 12:14:50 +0000

    writing function to file 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,7 +11,19 @@
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+<<<<<<< HEAD
 	if ((tree == NULL) || (tree->parent == NULL))
 		return (0);
 	return (binary_tree_depth(tree->parent) + 1);
+=======
+	size_t ret_value = 0;
+
+	if ((tree == NULL) || ((tree->left != NULL) && (tree->right != NULL)))
+		return (0);
+	if (tree->left == NULL)
+		ret_value++;
+	if (tree->right == NULL)
+		ret_value++;
+	return (ret_value);
+>>>>>>> dc52ab0 (writing function to file 10)
 }
